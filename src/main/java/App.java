737ad52243528bee138c1475a862bb87df3d8ab3
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-06-03 08:17:55 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        String name = myApp.readName();
+        String outputString = myApp.generateOutputString(name);
+        myApp.printOutput(outputString);
+    }
+
+    private void printOutput(String outputString) {
+        System.out.println(outputString);
+    }
+
+    private String readName() {
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+        return name;
+    }
+
+    private String generateOutputString(String name) {
+        return "Hello, " + name + ", nice to meet you!";
+    }
+}
